bdf90d8c93d7b3bf1262360e697daf985166507c
Shashank KATTI <90169675+Shashank26112@users.noreply.github.com>
2023-12-27 20:47:28 -0800

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            Console.WriteLine("enter the two number1:");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enerthe number2:");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            int result;
+            result = n1 + n2;
+            Console.WriteLine("Sum of the two numbers:" + result.ToString());
+            Console.ReadLine();
+        }
+    }
+}
